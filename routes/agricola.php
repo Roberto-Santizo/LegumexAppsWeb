4ef37d74d6357d834afef9b13455c73c01fda5d4
Roberto Acan Santizo <prestamotic@legumexadt.net>
2024-11-19 16:02:52 -0600

    bugfix(agricola route): cambia el middleware del rol para rutas de agricola

--- routes/agricola.php
+++ routes/agricola.php
@@ -83,7 +83,7 @@ Route::group(['middleware' => ['auth', 'role:admin|adminagricola'], 'prefix' => 
     Route::get('/finca/ingresos', [UsuariosFincaController::class, 'index'])->name('usuariosFincas');
 });
 
-Route::group(['middleware' => ['auth', 'role:admin|adminagricola|auxalameda'], 'prefix' => 'agricola'], function() {
+Route::group(['middleware' => ['auth', 'role:admin|adminagricola|auxfinca'], 'prefix' => 'agricola'], function() {
     Route::get('/tarea-lote-cosecha/{tarealotecosecha}', [TareaCosechaLoteController::class, 'show'])->name('planSemanal.tareaLoteCosecha.show');
 
     Route::get('/finca/plan-semanal/lotes/{lote:nombre}/{plansemanalfinca}/{tarea}/{tarealotecosecha}/cosecha/asignacion', [PlanSemanalFincasController::class, 'AsignarEmpleadosCosecha'])->name('planSemanal.AsignarEmpleadosCosecha');
