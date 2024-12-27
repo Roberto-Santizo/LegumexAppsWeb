b153b9586ef0fdf0d1b33243e5f2ca9fcc26ec72
Roberto Acan Santizo <prestamotic@legumexadt.net>
2024-12-27 10:18:02 -0600

    bugfix(carga dashboard agricola): fixea el tiempo de carga del dashboard agricola a la mitad

--- app/Livewire/DashboardAgricola.php
+++ app/Livewire/DashboardAgricola.php
@@ -8,8 +8,6 @@ use App\Models\EmpleadoFinca;
 use Illuminate\Support\Carbon;
 use App\Models\PlanSemanalFinca;
 use App\Models\TareaLoteCosecha;
-use App\Models\UsuarioTareaCosecha;
-use App\Models\UsuarioTareaLote;
 
 class DashboardAgricola extends Component
 {
@@ -28,8 +26,21 @@ class DashboardAgricola extends Component
     public $isOpen = false;
     public $finca = 0;
     public $horasDron = 0;
+    public $userRole = '';
 
     public function mount()
+    {
+        $this->userRole = auth()->user()->getRoleNames()->first();
+
+        if ($this->userRole == 'auxfinca') {
+            $this->finca = $this->getFincaId();
+        }
+
+        $this->buscarDatos();
+        $this->mostrarDatos();
+    }
+
+    private function getFincaId()
     {
         $PermisoFinca = [
             'tehuya' => 4,
@@ -37,122 +48,8 @@ class DashboardAgricola extends Component
         ];
         $user = auth()->user();
         $finca = $user->getAllPermissions()->first()->name;
-        $userRole = $user->getRoleNames()->first();
 
-        if ($userRole == 'auxfinca') {
-            $this->finca = $PermisoFinca[$finca];
-            $this->buscarDatos();
-        } else {
-            $this->semana_actual = Carbon::now()->weekOfYear;
-            $this->planesSelect = PlanSemanalFinca::all();
-            $this->planes = PlanSemanalFinca::where('semana', $this->semana_actual)->get();
-            $this->usuarios = EmpleadoFinca::whereNotIn('position_id', [15, 9])->get();
-            $this->tareas = TareasLote::all();
-            $this->tareasCosecha = TareaLoteCosecha::with('asignaciones')->get();
-        }
-
-        $this->mostrarDatos();
-    }
-
-    public function mostrarDatos()
-    {
-        $semana = $this->semana_actual;
-
-        $this->tareas->map(function($tarea){
-            if($tarea->asignacion && $tarea->asignacion->use_dron && $tarea->cierre){
-                $this->horasDron += round($tarea->asignacion->created_at->diffInHours($tarea->cierre->created_at),2);
-            }
-        });
-        
-        $this->planes->map(function ($plan) {
-            $plan->tareasRealizadas = $plan->tareasTotales->filter(function ($tarea) {
-                return $tarea->cierre;
-            });
-        });
-
-        $this->usuarios->map(function ($usuario) use ($semana) {
-            $horas_totales_cosecha = 0;
-            $asignaciones = UsuarioTareaLote::whereRaw('WEEKOFYEAR(created_at) = ?', [$semana])
-                ->where('usuario_id', $usuario->id)
-                ->get();
-
-            $asignacionesCosecha = UsuarioTareaCosecha::whereRaw('WEEKOFYEAR(created_at) = ?', [$semana])
-                ->where('usuario_id', $usuario->id)
-                ->get();
-
-            $asignacionesCosechaHoy = UsuarioTareaCosecha::whereDate('created_at', Carbon::today())
-                ->where('usuario_id', $usuario->id)
-                ->get();
-
-            $horas_totales_tareas = $asignaciones->sum(function ($asignacion) {
-                return round($asignacion->tarea_lote->horas / $asignacion->tarea_lote->users->count(), 2);
-            });
-
-            foreach ($asignacionesCosecha as $asignacionCosecha) {
-                $cierre = $asignacionCosecha->tarealote
-                    ->cierreDiario($asignacionCosecha->created_at)
-                    ->get()
-                    ->first();
-                if ($cierre) {
-                    $libras_planta = $cierre->libras_total_planta;
-                    $libras_finca = $cierre->libras_total_finca;
-                    if ($libras_planta) {
-                        $plantas_cosechadas = $cierre->plantas_cosechadas;
-                        $peso_cabeza = $libras_planta / $plantas_cosechadas;
-                        $porcentaje = $asignacionCosecha->libras_asignacion / $libras_finca;
-                        $cabezas_cosechadas = ($porcentaje * $libras_planta) / $peso_cabeza;
-                        $horas_totales_cosecha += $cabezas_cosechadas / 120;
-                    }
-                }
-            }
-
-            $usuario->horas_totales = round($horas_totales_tareas + $horas_totales_cosecha, 2);
-
-            foreach ($asignaciones as $asignacion) {
-                if (!$asignacion->tarea_lote->cierre) {
-                    $usuario->activo = true;
-                }
-            }
-
-            foreach ($asignacionesCosechaHoy as $asignacion) {
-                if (!$asignacion->tarealote->cierreDiario) {
-                    $usuario->activo = true;
-                }
-            }
-        });
-
-        $this->usuarios = $this->usuarios->sortBy('horas_totales');
-
-        $this->tareasCosecha = $this->tareasCosecha->filter(function ($tarea) use ($semana) {
-            return $tarea->plansemanal->semana == $semana;
-        });
-
-        $this->tareasEnProceso = $this->tareas->filter(function ($tarea) {
-            return $tarea->plansemanal->semana == $this->semana_actual &&
-                $tarea->asignacion &&
-                !$tarea->cierre;
-        });
-
-        $this->tareasRealizadas = $this->tareas->filter(function ($tarea) {
-            return $tarea->cierre;
-        });
-
-        $this->tareasRealizadasEnSemana = $this->tareasRealizadas->filter(function ($tarea) use ($semana) {
-            return $tarea->cierre && $tarea->cierre->created_at->weekOfYear == $semana;
-        });
-
-    }
-
-    public function openModal()
-    {
-        $this->isOpen = !$this->isOpen;
-    }
-
-    public function borrarFiltros()
-    {
-        $this->semanaNueva = null;
-        $this->finca = 0;
-        $this->mostrarDatos();
+        return $PermisoFinca[$finca];
     }
 
     public function buscarDatos()
@@ -183,7 +80,62 @@ class DashboardAgricola extends Component
         }
 
         $this->mostrarDatos();
-        $this->openModal();
+        $this->closeModal();
+    }
+
+    public function mostrarDatos()
+    {
+        $semana = $this->semana_actual;
+
+        $this->tareas->map(function ($tarea) {
+            if ($tarea->asignacion && $tarea->asignacion->use_dron && $tarea->cierre) {
+                $this->horasDron += round($tarea->asignacion->created_at->diffInHours($tarea->cierre->created_at), 2);
+            }
+        });
+
+        $this->planes->map(function ($plan) {
+            $plan->tareasRealizadas = $plan->tareasTotales->filter(function ($tarea) {
+                return $tarea->cierre;
+            });
+        });
+
+
+        $this->tareasCosecha = $this->tareasCosecha->filter(function ($tarea) use ($semana) {
+            return $tarea->plansemanal->semana == $semana;
+        });
+
+        $this->tareasEnProceso = $this->tareas->filter(function ($tarea) {
+            return $tarea->plansemanal->semana == $this->semana_actual &&
+                $tarea->asignacion &&
+                !$tarea->cierre;
+        });
+
+        $this->tareasRealizadas = $this->tareas->filter(function ($tarea) {
+            return $tarea->cierre;
+        });
+
+        $this->tareasRealizadasEnSemana = $this->tareasRealizadas->filter(function ($tarea) use ($semana) {
+            return $tarea->cierre && $tarea->cierre->created_at->weekOfYear == $semana;
+        });
+    }
+
+
+    public function openModal()
+    {
+        $this->isOpen = true;
+    }
+
+    public function closeModal()
+    {
+        $this->isOpen = false;
+    }
+
+    public function borrarFiltros()
+    {
+        $this->semanaNueva = null;
+        $this->finca = 0;
+        $this->mostrarDatos();
+        $this->closeModal();
     }
 
     public function render()
