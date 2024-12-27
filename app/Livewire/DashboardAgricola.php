b404509ac8163e4baaa3d49b1a0c9f5c0c27f6f4
Roberto Acan Santizo <prestamotic@legumexadt.net>
2024-12-27 11:09:26 -0600

    bugfix(horas usuarios): fixea las horas de los usuarios en el dashboard agricola

--- app/Livewire/DashboardAgricola.php
+++ app/Livewire/DashboardAgricola.php
@@ -8,6 +8,8 @@ use App\Models\EmpleadoFinca;
 use Illuminate\Support\Carbon;
 use App\Models\PlanSemanalFinca;
 use App\Models\TareaLoteCosecha;
+use App\Models\UsuarioTareaLote;
+use App\Models\UsuarioTareaCosecha;
 
 class DashboardAgricola extends Component
 {
@@ -100,6 +102,58 @@ class DashboardAgricola extends Component
         });
 
 
+        $this->usuarios->map(function ($usuario) use ($semana) {
+            $horas_totales_cosecha = 0;
+            $asignaciones = UsuarioTareaLote::whereRaw('WEEKOFYEAR(created_at) = ?', [$semana])
+                ->where('usuario_id', $usuario->id)
+                ->get();
+
+            $asignacionesCosecha = UsuarioTareaCosecha::whereRaw('WEEKOFYEAR(created_at) = ?', [$semana])
+                ->where('usuario_id', $usuario->id)
+                ->get();
+
+            $asignacionesCosechaHoy = UsuarioTareaCosecha::whereDate('created_at', Carbon::today())
+                ->where('usuario_id', $usuario->id)
+                ->get();
+
+            $horas_totales_tareas = $asignaciones->sum(function ($asignacion) {
+                return round($asignacion->tarea_lote->horas / $asignacion->tarea_lote->users->count(), 2);
+            });
+
+            foreach ($asignacionesCosecha as $asignacionCosecha) {
+                $cierre = $asignacionCosecha->tarealote
+                    ->cierreDiario($asignacionCosecha->created_at)
+                    ->get()
+                    ->first();
+                if ($cierre) {
+                    $libras_planta = $cierre->libras_total_planta;
+                    $libras_finca = $cierre->libras_total_finca;
+                    if ($libras_planta) {
+                        $plantas_cosechadas = $cierre->plantas_cosechadas;
+                        $peso_cabeza = $libras_planta / $plantas_cosechadas;
+                        $porcentaje = $asignacionCosecha->libras_asignacion / $libras_finca;
+                        $cabezas_cosechadas = ($porcentaje * $libras_planta) / $peso_cabeza;
+                        $horas_totales_cosecha += $cabezas_cosechadas / 120;
+                    }
+                }
+            }
+
+            $usuario->horas_totales = round($horas_totales_tareas + $horas_totales_cosecha, 2);
+
+            foreach ($asignaciones as $asignacion) {
+                if (!$asignacion->tarea_lote->cierre) {
+                    $usuario->activo = true;
+                }
+            }
+
+            foreach ($asignacionesCosechaHoy as $asignacion) {
+                if (!$asignacion->tarealote->cierreDiario) {
+                    $usuario->activo = true;
+                }
+            }
+        });
+
+        $this->usuarios = $this->usuarios->sortBy('horas_totales');
         $this->tareasCosecha = $this->tareasCosecha->filter(function ($tarea) use ($semana) {
             return $tarea->plansemanal->semana == $semana;
         });
